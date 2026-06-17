21ebe32de36f16506ca7200d45e33ccba5f50953
SynOrq <tarikvcelik@gmail.com>
2026-06-17 22:17:08 +0300

    fix(api): import auth module for protected trip and shift routes

--- apps/api/src/trips/trips.module.ts
+++ apps/api/src/trips/trips.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ShiftsModule } from '../shifts/shifts.module';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
 
 @Module({
-  imports: [PrismaModule, ShiftsModule],
+  imports: [AuthModule, PrismaModule, ShiftsModule],
   controllers: [TripsController],
   providers: [TripsService],
   exports: [TripsService]
